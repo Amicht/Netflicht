0f7c921d1f064ca5ece80bd3e60c90277b8e6ee6
Amicht <vicolicht@gmail.com>
2022-09-01 13:11:47 +0300

    sync repo

--- backend/generateData/generateData.mjs
+++ backend/generateData/generateData.mjs
@@ -10,35 +10,6 @@ const DATABASE = 'netflicht';
 
 mongoose.connect(HOSTURL + DATABASE);
 
-const users = [
-    {
-        name:"עמית",
-        img: "/api/assets/images/profiles/1.png",
-        watchList: []
-    },
-    {
-        name:"טליה",
-        img: "/api/assets/images/profiles/2.png",
-        watchList: []
-    },
-    {
-        name:"תומר",
-        img: "/api/assets/images/profiles/3.png",
-        watchList: []
-    },
-    {
-        name:"אבא",
-        img: "/api/assets/images/profiles/4.png",
-        watchList: []
-    },
-    {
-        name:"ילדים",
-        img: "/api/assets/images/profiles/5.png",
-        watchList: []
-    }
-]
-
-const db = shuffle([...movieDb, ...seariesDb]);
 
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
@@ -58,6 +29,6 @@ function shuffle(array) {
     return array;
 }
 
-
+// const db = shuffle([...movieDb, ...seariesDb]);
 // addMovies(db);
 // addUsers(users).then(res => console.log(res));
